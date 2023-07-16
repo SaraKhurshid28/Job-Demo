df03d92e07425151b5138d9180c528d9233fe65f
Sara Khurshid <Sara.Khurshid@zweidevs.com>
2023-07-17 00:56:47 +0500

    add firebase file

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCZO77raVduqIJkfMBXMTolqDwsdGvKER0",
+  authDomain: "job-demo-cecd5.firebaseapp.com",
+  projectId: "job-demo-cecd5",
+  storageBucket: "job-demo-cecd5.appspot.com",
+  messagingSenderId: "921798864300",
+  appId: "1:921798864300:web:acb25fd44b5b107f541e09",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
